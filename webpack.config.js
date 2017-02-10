6588a733767d2642926c099b0be92baee00eb13d
Seven <seven@megalive.com.tw>
2017-02-10 12:07:15 +0800

    Modified to webpack2 config

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,47 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        bundle: [
-            'webpack-dev-server/client?http://localhost:8000',
-            'webpack/hot/dev-server',
-            './example/index.jsx'
-        ]
-    },
-    devServer: {
-       host: 'localhost',
-       port: 8000,
-       contentBase: './example'
-    },
-	output: {
-        path: path.join(__dirname, 'example'),
-		filename: 'bundle.js',
-	},
-    plugins:[new webpack.HotModuleReplacementPlugin()],
-	resolveLoader: {
-		modulesDirectories: ['node_modules']
-	},
-	resolve: {
-		extensions: ['', '.js', 'jsx']
-	},
-	module: {
-		loaders: [
-            {
-                test: /\.js[x]?$/,
-                exclude: /(node_modules)/,
-                loaders: ['babel?cacheDirectory=true']
-            }
-        ]
-  }
+  entry: {
+    bundle: [
+      'webpack/hot/dev-server',
+      './example/index.jsx',
+    ],
+  },
+  devServer: {
+    host: '0.0.0.0',
+    port: 8000,
+    contentBase: './example',
+  },
+  output: {
+    path: path.join(__dirname, 'example'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      minimize: false,
+      debug: true,
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', 'jsx'],
+    modules: ['node_modules'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
--- webpack.prov.config.js
+++ webpack.prov.config.js
@@ -2,34 +2,53 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-        entry: {
-            "bundle": './example/index.jsx'
-        },
-        output: {
-            path: path.join(__dirname, 'docs'),
-      		  filename: 'bundle.js',
-      	},
-        plugins: [
-            new webpack.NoErrorsPlugin(),
-            new webpack.DefinePlugin({
-                'process.env.NODE_ENV': JSON.stringify('production')
-            }),
-            new webpack.optimize.UglifyJsPlugin({
-              compress: {
-                warnings: false
-              }
-            })
+  entry: {
+    bundle: './example/index.jsx',
+  },
+  output: {
+    path: path.join(__dirname, 'docs'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        unused: true,
+        dead_code: true,
+      },
+      output: {
+        comments: false,
+      },
+    }),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+  ],
+  resolve: {
+    extensions: ['.js', 'jsx'],
+    modules: ['node_modules'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+            },
+          },
         ],
-        resolve: {
-      		extensions: ['', '.js', 'jsx']
-      	},
-      	module: {
-      		loaders: [
-                  {
-                      test: /\.js[x]?$/,
-                      exclude: /(node_modules)/,
-                      loaders: ['babel?cacheDirectory=true']
-                  }
-              ]
-        }
-    };
+      },
+    ],
+  },
+};
